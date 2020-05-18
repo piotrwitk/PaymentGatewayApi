f3a80466fc144e593a1a96ee0c29f66d224c5575
Piotr Witkowski <ps2dev@wp.pl>
2020-05-18 23:21:46 +0100

    Added basic API health check implementation

---
+++ PaymentGateway/PaymentGateway.WebApi/ApiHealthCheck.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.WebApi
+{
+    public class ApiHealthCheck : IHealthCheck
+    {
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            return Task.FromResult(HealthCheckResult.Healthy("healthy"));
+        }
+    }
+}
