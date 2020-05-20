f4cb43bc55d30ee942ac36fb3b6dc278ffdc76eb
Piotr Witkowski <ps2dev@wp.pl>
2020-05-20 10:53:48 +0100

    - Added dumy payment controller - Added GatewayClock - Added V1 request mapper (for details)

---
+++ PaymentGateway/PaymentGateway.WebApi/Models/v1/RequestMapper.cs
@@ -0,0 +1,27 @@
+﻿using PaymentGateway.Models;
+using PaymentGateway.WebApi.Utils;
+using System;
+
+namespace PaymentGateway.WebApi.Models.v1
+{
+    public static class RequestMapper
+    {
+        public static GatewayDetailsRequest MapDetailsRequest(string merchantId, string merchantReferenceNumber, IGatewayClock clock)
+        {
+
+            if (string.IsNullOrEmpty(merchantId) || string.IsNullOrEmpty(merchantReferenceNumber))
+            {
+                throw new ArgumentException("Missing: " + 
+                    (string.IsNullOrEmpty(merchantId) ? "merchant id " : "") +
+                    (string.IsNullOrEmpty(merchantId) ? "merchant reference number " : ""));
+            }
+
+            return new GatewayDetailsRequest
+            {
+                MerchantId = merchantId,
+                MerchantReferenceNumber = merchantReferenceNumber,
+                TimeStamp = clock.GetCurrentUtcTimestamp()
+            };
+        }
+    }
+}
---
+++ PaymentGateway/PaymentGateway/PaymentProcessors/SimulatedPaymentProcessor.cs
@@ -0,0 +1,14 @@
+﻿using PaymentGateway.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace PaymentGateway.PaymentProcessors
+{
+    public class SimulatedPaymentProcessor : IPaymentProcessor
+    {
+        public Task<GatewayPaymentResponse> HandlePaymentRequest(GatewayPaymentRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
